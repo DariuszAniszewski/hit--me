8e8823e040a062128abcbb797ac88f35309d985b
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2016-01-03 14:29:23 +0100

    authorize blitz.io

--- hit--me.py
+++ hit--me.py
@@ -10,6 +10,11 @@ def hello_world():
     return 'Hit Me application for load testing demonstrations'
 
 
+@app.route('/mu-012768e9-7817db3b-7bc502ac-f9ff267a')
+def blitz_io():
+    return "42"
+
+
 @app.route('/<int:millis>ms')
 def delay_for_amount_of_millis(millis):
     sleep(millis / 1000)
