b428f5a7c82566444c9471b1c7652397b93576b0
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2016-01-04 10:55:32 +0100

    added request that sleeps longer with more requests

--- hit--me.py
+++ hit--me.py
@@ -1,8 +1,10 @@
 from time import sleep
 
 from flask import Flask
+from flask.ext.cache import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 
 @app.route('/')
@@ -19,6 +21,17 @@ def blitz_io():
 def delay_for_amount_of_millis(millis):
     sleep(millis / 1000)
     return "This request took like {}ms".format(millis)
+
+
+@app.route('/increment')
+def incremental_delay():
+    cache_key = "milliseconds_to_sleep"
+    milliseconds_to_sleep = cache.get(cache_key)
+    if not milliseconds_to_sleep:
+        milliseconds_to_sleep = 0
+    sleep(1.0 * milliseconds_to_sleep / 1000)
+    cache.set(cache_key, milliseconds_to_sleep + 10, timeout=60)
+    return "This request took like {}ms. Next one will be longer".format(milliseconds_to_sleep)
 
 
 if __name__ == '__main__':
